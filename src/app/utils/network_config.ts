3a51587b298b7bb44d4b600074a237dd182333ef
Deepak Singh <113917784+DEEPAKsingh74@users.noreply.github.com>
2024-10-03 22:00:02 +0530

    Update network_config.ts

--- src/app/utils/network_config.ts
+++ src/app/utils/network_config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // axios base configurations
-const BASE_URL = process.env.REQUEST_BASE_URL;
+const BASE_URL = process.env.NEXT_PUBLIC_REQUEST_BASE_URL;
 
 export const axiosInstance = axios.create({
   baseURL: `${BASE_URL}`,	
